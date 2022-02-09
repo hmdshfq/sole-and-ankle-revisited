f8bc62c019cf21b0d632a1d1b4c95380eae13ef3
Hammad <hmdshfq@outlook.com>
2022-02-09 22:24:09 +0100

    add media queries and fix header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,73 +1,128 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { COLORS, WEIGHTS } from '../../constants';
+import { COLORS, WEIGHTS, QUERIES } from '../../constants';
 import Logo from '../Logo';
+import Icon from '../Icon';
+import UnstyledButton from '../UnstyledButton';
 import SuperHeader from '../SuperHeader';
 import MobileMenu from '../MobileMenu';
+import VisuallyHidden from '../VisuallyHidden';
 
 const Header = () => {
-  const [showMobileMenu, setShowMobileMenu] = React.useState(false);
+    const [showMobileMenu, setShowMobileMenu] = React.useState(false);
 
-  // For our mobile hamburger menu, we'll want to use a button
-  // with an onClick handler, something like this:
-  //
-  // <button onClick={() => setShowMobileMenu(true)}>
+    // For our mobile hamburger menu, we'll want to use a button
+    // with an onClick handler, something like this:
+    //
+    // <button onClick={() => setShowMobileMenu(true)}>
 
-  return (
-    <header>
-      <SuperHeader />
-      <MainHeader>
-        <Side>
-          <Logo />
-        </Side>
-        <Nav>
-          <NavLink href="/sale">Sale</NavLink>
-          <NavLink href="/new">New&nbsp;Releases</NavLink>
-          <NavLink href="/men">Men</NavLink>
-          <NavLink href="/women">Women</NavLink>
-          <NavLink href="/kids">Kids</NavLink>
-          <NavLink href="/collections">Collections</NavLink>
-        </Nav>
-        <Side />
-      </MainHeader>
+    return (
+        <header>
+            <SuperHeader />
+            <MainHeader>
+                <LogoWrapper>
+                    <Logo />
+                </LogoWrapper>
+                <DesktopNav>
+                    <NavLink href='/sale'>Sale</NavLink>
+                    <NavLink href='/new'>New&nbsp;Releases</NavLink>
+                    <NavLink href='/men'>Men</NavLink>
+                    <NavLink href='/women'>Women</NavLink>
+                    <NavLink href='/kids'>Kids</NavLink>
+                    <NavLink href='/collections'>Collections</NavLink>
+                </DesktopNav>
+                <MobileActions>
+                    <UnstyledButton>
+                        <Icon id='shopping-bag'></Icon>
+                        <VisuallyHidden>Open Cart</VisuallyHidden>
+                    </UnstyledButton>
+                    <UnstyledButton>
+                        <Icon id='search'></Icon>
+                        <VisuallyHidden>Search</VisuallyHidden>
+                    </UnstyledButton>
+                    <UnstyledButton>
+                        <Icon id='menu'></Icon>
+                        <VisuallyHidden>Menu</VisuallyHidden>
+                    </UnstyledButton>
+                </MobileActions>
+                <Filler />
+            </MainHeader>
 
-      <MobileMenu
-        isOpen={showMobileMenu}
-        onDismiss={() => setShowMobileMenu(false)}
-      />
-    </header>
-  );
+            <MobileMenu
+                isOpen={showMobileMenu}
+                onDismiss={() => setShowMobileMenu(false)}
+            />
+        </header>
+    );
 };
 
 const MainHeader = styled.div`
-  display: flex;
-  align-items: baseline;
-  padding: 18px 32px;
-  height: 72px;
-  border-bottom: 1px solid ${COLORS.gray[300]};
+    display: flex;
+    align-items: baseline;
+    padding: 18px 32px;
+    height: 72px;
+    border-bottom: 1px solid ${COLORS.gray[300]};
+    border-top: 4px solid ${COLORS.gray[900]};
+
+    @media ${QUERIES.tabletAndSmaller} {
+        justify-content: space-between;
+        align-items: center;
+    }
+    @media ${QUERIES.phoneAndSmaller} {
+        padding-left: 16px;
+        padding-right: 16px;
+    }
 `;
 
-const Nav = styled.nav`
-  display: flex;
-  gap: 48px;
-  margin: 0px 48px;
+const DesktopNav = styled.nav`
+    display: flex;
+    gap: 48px;
+    margin: 0px 48px;
+
+    @media ${QUERIES.tabletAndSmaller} {
+        display: none;
+    }
 `;
 
-const Side = styled.div`
-  flex: 1;
+const MobileActions = styled.div`
+    display: none;
+
+    @media ${QUERIES.tabletAndSmaller} {
+        display: flex;
+        gap: 32px;
+    }
+    @media ${QUERIES.phoneAndSmaller} {
+        gap: 16px;
+    }
+`;
+
+const LogoWrapper = styled.div`
+    flex: 1;
+
+    @media ${QUERIES.tabletAndSmaller} {
+        flex: revert;
+    }
+`;
+
+const Filler = styled.div`
+    flex: 1;
+
+    @media ${QUERIES.tabletAndSmaller} {
+        display: none;
+    }
 `;
 
 const NavLink = styled.a`
-  font-size: 1.125rem;
-  text-transform: uppercase;
-  text-decoration: none;
-  color: ${COLORS.gray[900]};
-  font-weight: ${WEIGHTS.medium};
+    font-size: 1.125rem;
+    text-transform: uppercase;
+    text-decoration: none;
+    color: ${COLORS.gray[900]};
+    font-weight: ${WEIGHTS.medium};
 
-  &:first-of-type {
-    color: ${COLORS.secondary};
-  }
+    &:first-of-type {
+        color: ${COLORS.secondary};
+    }
 `;
 
 export default Header;
