6d76e3d7f5499c6fc79a781a10c72b826308bd7d
Hammad <hmdshfq@outlook.com>
2022-02-14 11:28:25 +0100

    Complete mobile menu modal

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -3,35 +3,94 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import { DialogOverlay, DialogContent } from '@reach/dialog';
 
-import { QUERIES } from '../../constants';
+import { COLORS, QUERIES, WEIGHTS } from '../../constants';
 
 import UnstyledButton from '../UnstyledButton';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
 const MobileMenu = ({ isOpen, onDismiss }) => {
-  if (!isOpen) {
-    return null;
-  }
-
-  return (
-    <div>
-      <button onClick={onDismiss}>Dismiss menu</button>
-      <nav>
-        <a href="/sale">Sale</a>
-        <a href="/new">New&nbsp;Releases</a>
-        <a href="/men">Men</a>
-        <a href="/women">Women</a>
-        <a href="/kids">Kids</a>
-        <a href="/collections">Collections</a>
-      </nav>
-      <footer>
-        <a href="/terms">Terms and Conditions</a>
-        <a href="/privacy">Privacy Policy</a>
-        <a href="/contact">Contact Us</a>
-      </footer>
-    </div>
-  );
+    return (
+        <Overlay isOpen={isOpen} onDismiss={onDismiss}>
+            <Content>
+                <CloseButton onClick={onDismiss}>
+                    <Icon id='close'></Icon>
+                    <VisuallyHidden>Dismiss menu</VisuallyHidden>
+                </CloseButton>
+                <Filler />
+                <Nav>
+                    <NavLink href='/sale'>Sale</NavLink>
+                    <NavLink href='/new'>New&nbsp;Releases</NavLink>
+                    <NavLink href='/men'>Men</NavLink>
+                    <NavLink href='/women'>Women</NavLink>
+                    <NavLink href='/kids'>Kids</NavLink>
+                    <NavLink href='/collections'>Collections</NavLink>
+                </Nav>
+                <Footer>
+                    <FooterLink href='/terms'>Terms and Conditions</FooterLink>
+                    <FooterLink href='/privacy'>Privacy Policy</FooterLink>
+                    <FooterLink href='/contact'>Contact Us</FooterLink>
+                </Footer>
+            </Content>
+        </Overlay>
+    );
 };
 
+const Overlay = styled(DialogOverlay)`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: hsl(220 5% 40% / 0.8);
+    display: flex;
+    justify-content: flex-end;
+`;
+
+const Content = styled(DialogContent)`
+    background: white;
+    width: 300px;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+`;
+const CloseButton = styled(UnstyledButton)`
+    position: absolute;
+    top: 16px;
+    right: 0;
+    padding: 16px;
+`;
+const Nav = styled.nav`
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    padding: 32px;
+`;
+const NavLink = styled.a`
+    text-transform: uppercase;
+    color: ${COLORS.gray[900]};
+    font-size: 1.125rem;
+    font-weight: ${WEIGHTS.medium};
+    text-decoration: none;
+
+    &:first-of-type {
+        color: ${COLORS.secondary};
+    }
+`;
+const Footer = styled.footer`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    padding: 32px;
+    gap: 6px;
+`;
+const FooterLink = styled.a`
+    color: ${COLORS.gray[700]};
+    text-decoration: none;
+    font-size: 0.875rem;
+`;
+const Filler = styled.div`
+    flex: 1;
+`;
 export default MobileMenu;
